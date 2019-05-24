6d50fe80d036a3a28d035adcf13ff72b752f4ec9
BobiNedelchev <32366333+bozhko10@users.noreply.github.com>
2019-05-24 22:21:15 +0100

    Variables names

--- DoubleM.java
+++ DoubleM.java
@@ -5,48 +5,48 @@ public class DoubleM
 {
     // Instance variables
     private int N;
-    private int StartEndN;
-    private int StarsMiddle;
-    private int DashMiddle;
+    private int startEndN;
+    private int starsMiddle;
+    private int dashMiddle;
 
     // Constructor
     public DoubleM(int givenN)
     {
         N = givenN;
-        StartEndN = N;
-        StarsMiddle = N;
-        DashMiddle = N;
+        startEndN = N;
+        starsMiddle = N;
+        dashMiddle = N;
     } // DoubleM
 
     // Printing method
     public void print()
     {
         // While printing the first half of the letter
-        while (DashMiddle >= 1) 
+        while (dashMiddle >= 1) 
         {
             for (int i = 0; i < 10; i++) 
             {
                 if (i == 0 || i == 4 || i == 5 || i == 9)
-                    for (int j = 0; j < StartEndN; j++)
+                    for (int j = 0; j < startEndN; j++)
                         System.out.print("-");
                 else if (i == 2 || i == 7)
-                    for (int j = 0; j < DashMiddle; j++)
+                    for (int j = 0; j < dashMiddle; j++)
                         System.out.print("-");
                 else
-                    for (int j = 0; j < StarsMiddle; j++)
+                    for (int j = 0; j < starsMiddle; j++)
                         System.out.print("*");
             } // for
             System.out.println();
             // Decrementing/Incrementing the variables after each printed line
-            StartEndN--;
-            StarsMiddle += 2;
-            DashMiddle -= 2;
+            startEndN--;
+            starsMiddle += 2;
+            dashMiddle -= 2;
         } // while
         // Setting the variables for the next half
-        DashMiddle = 1;
-        StarsMiddle -= 2;
+        dashMiddle = 1;
+        starsMiddle -= 2;
         // Second while loop printing the second half of the letter
-        while (DashMiddle <= N) 
+        while (dashMiddle <= N) 
         {
             for (int i = 0; i < 14; i++) 
             {
@@ -54,20 +54,20 @@ public class DoubleM
                     for (int j = 0; j < N; j++)
                         System.out.print("*");
                 else if (i == 2 || i == 4 || i == 9 || i == 11)
-                    for (int j = 0; j < DashMiddle; j++)
+                    for (int j = 0; j < dashMiddle; j++)
                         System.out.print("-");
                 else if (i == 3 || i == 10)
-                    for (int j = 0; j < StarsMiddle; j++)
+                    for (int j = 0; j < starsMiddle; j++)
                         System.out.print("*");
                 else
-                    for (int j = 0; j < StartEndN; j++)
+                    for (int j = 0; j < startEndN; j++)
                         System.out.print("-");
             } // for
             System.out.println();
             // Decrementing/Incrementing the variables after each printed line
-            StartEndN--;
-            StarsMiddle -= 2;
-            DashMiddle += 2;
+            startEndN--;
+            starsMiddle -= 2;
+            dashMiddle += 2;
         } // while 
     } // print()
 } // class DoubleM 
